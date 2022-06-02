eb5769c465c9caabdc8b589d0e07ae9df80f51bf
Mustafaali2727 <106687249+Mustafaali2727@users.noreply.github.com>
2022-06-02 16:49:51 +0300

    Update song.py

--- handlers/song.py
+++ handlers/song.py
@@ -56,7 +56,6 @@ def bul(client, message):
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
         rep = f"☑️ **İsmi**: [{title[:35]}]({link})\n🎬 **Kaynak**: YouTube\n⏱️ **Süre**: `{duration}`\n👁‍🗨 **Görünümler**: `{views}`\n📤 **Tarafından**: @{BOT_USERNAME}"
-        secmul , dur , dur_arr  =  1 , 0 , süre . bölme ( ":" )"
         secmul, dur, dur_arr = 1, 0, duration.split(":")
         for i in range(len(dur_arr) - 1, -1, -1):
             dur += int(dur_arr[i]) * secmul
