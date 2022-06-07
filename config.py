e4373dbb3052aa3342343e949f8c1306076c7438
Efsane2323 <106430183+Efsane2323@users.noreply.github.com>
2022-06-07 15:50:43 +0300

    Update config.py

--- config.py
+++ config.py
@@ -7,10 +7,10 @@ admins = {}
 
 SESSION_NAME = getenv("SESSION_NAME", "session")
 BOT_TOKEN = getenv("BOT_TOKEN")
-BOT_NAME = getenv("BOT_NAME", "Musical") 
+BOT_NAME = getenv("BOT_NAME", "HABABAMSİNİFİMUSİC") 
 API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
-BOT_USERNAME = getenv("BOT_USERNAME", "musical_musiccbot")
+BOT_USERNAME = getenv("BOT_USERNAME", "hababam_sinifibot")
 PMPERMIT = getenv("PMPERMIT", "ENABLE")
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "70"))
 
