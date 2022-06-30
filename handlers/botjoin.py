e9c0fc4b489ff24667ea9e98c5a8151431be55ec
Efsane2323 <106430183+Efsane2323@users.noreply.github.com>
2022-06-30 09:32:59 +0300

    Update botjoin.py

--- handlers/botjoin.py
+++ handlers/botjoin.py
@@ -20,7 +20,7 @@ async def addchannel(client, message):
     try:
         user = await USER.get_me()
     except:
-        user.first_name =  "Sesmusic Asistan"
+        user.first_name =  "hababammusicAsistann"
 
     try:
         await USER.join_chat(invitelink)
